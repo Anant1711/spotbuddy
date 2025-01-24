032331d725aaa6419e35651c46989ab720dd5924
ACHAUHA2 <achauha2@visteon.com>
2025-01-24 23:26:36 +0530

    Add Auth-Checker in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,88 @@
+/**************************** Imports ****************************/
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:spotbuddy/firebase_options.dart';
+import 'package:spotbuddy/screens/AuthScreen.dart';
+/**************************** Imports ****************************/
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,);
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+    );
+    // await FirebaseAppCheck.instance.activate(
+    //   // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
+    //   // argument for `webProvider`
+    //   webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+    //   // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
+    //   // your preferred provider. Choose from:
+    //   // 1. Debug provider
+    //   // 2. Safety Net provider
+    //   // 3. Play Integrity provider
+    //   androidProvider: AndroidProvider.debug,
+    //   // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
+    //   // your preferred provider. Choose from:
+    //   // 1. Debug provider
+    //   // 2. Device Check provider
+    //   // 3. App Attest provider
+    //   // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
+    //   appleProvider: AppleProvider.appAttest,
+    // );
+  } catch (e) {
+    print("Error initializing Firebase: $e");
+  }
+  runApp(const SpotBuddyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SpotBuddyApp extends StatelessWidget {
+  const SpotBuddyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      color: const Color(0xffDCDBE2),
+      theme: _buildTheme(Brightness.light),
+      title: 'SpotBuddy',
+      home: AuthenticationWrapper(),
+      routes: {
+        '/home':(context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        'AuthScreen': (context) => Authscreen(),
+      },
+    );
+  }
+
+  ThemeData _buildTheme(brightness) {
+    var baseTheme = ThemeData(brightness: brightness);
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class AuthenticationWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LoadingAnimationWidget.fallingDot(color: const Color(0xff2226BA), size: 50);
+        } else if (snapshot.hasData) {
+          return HomeScreen(); // Use HomeScreen here
+        } else {
+          return Authscreen(); // Show AuthScreen for unauthenticated users
+        }
+      },
     );
   }
 }
